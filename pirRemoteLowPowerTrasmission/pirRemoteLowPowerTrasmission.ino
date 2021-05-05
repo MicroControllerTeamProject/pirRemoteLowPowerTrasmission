83fe07872dffc9faf75290df6105a481d326b084
Luigi Santagada <luigi.santagada@live.com>
2021-05-05 12:41:58 +0200

    .

---
+++ pirRemoteLowPowerTrasmission/pirRemoteLowPowerTrasmission.ino
@@ -0,0 +1,115 @@
+#include <SoftwareSerial.h>
+#include <VirtualWire.h> //Download it here: http://electronoobs.com/eng_arduino_virtualWire.php
+#include <avr/sleep.h>
+//SoftwareSerial mySerial(666, 3);
+
+bool isSystemActivated = false;
+
+byte adcsra;
+
+byte transistorPin = 0;
+
+void setup() {
+	setVirtualWireForRF433Receiver();
+	//mySerial.begin(9600);
+	vw_set_ptt_pin(999);
+	vw_set_ptt_inverted(false);
+	vw_set_tx_pin(4);
+	vw_setup(100); // Bits per sec
+
+	pinMode(0, OUTPUT);
+	//pinMode(1, OUTPUT);
+	PCMSK |= bit(PCINT2);  // want pin D3 / pin 2
+	GIFR |= bit(PCIF);    // clear any outstanding interrupts
+	GIMSK |= bit(PCIE);    // enable pin change interrupts
+
+	/*randomSeed(analogRead(A0));*/
+
+	attachInterrupt(0, activateSystemInterrupt, CHANGE);
+}
+
+void activateSystemInterrupt()
+{
+	isSystemActivated = true;
+}
+long number = 0;
+
+bool canGo = true;
+
+void loop() {
+	uint8_t message[1];
+	uint8_t messageLength = 1; // the size of the message
+
+	/*number = random(1, 5);*/
+
+	if (isSystemActivated)
+	{
+		for (int i = 0; i < 5; i++)
+		{
+			canGo = true;
+			for (int i = 0; i < 100; i++)
+			{
+				delay(500);
+				if (vw_get_message(message, &messageLength)) // non-blocking
+				{
+					for (int i = 0; i < messageLength; i++)
+					{
+						if ((char)message[i] == 'A')
+						{
+							for (int i = 0; i < 20; i++)
+							{
+								digitalWrite(0, HIGH);
+								delay(500);
+								digitalWrite(0, LOW);
+								delay(500);
+							}
+							//_sendMessageTime = 0;
+							delay(50000);
+							canGo = false;
+						}
+						//received_number = message[i];
+					}
+				}
+			}
+			if (canGo)
+			{
+				char message[1];// = "A";   // array to hold the result.
+				strcpy(message, "B"); // copy string one into the result.
+				vw_send((uint8_t*)message, 1);
+				vw_wait_tx();
+				digitalWrite(0, HIGH);   // turn the LED on (HIGH is the voltage level)
+				delay(10000);                       // wait for a second
+				digitalWrite(0, LOW);    // turn the LED off by making the voltage LOW}
+			}
+
+			//}
+		}
+		//mySerial.println("System sleep mode");
+		//delay(100000);
+		if (canGo)
+		{
+			isSystemActivated = false;
+			digitalWrite(0, HIGH);   // turn the LED on (HIGH is the voltage level)
+			delay(50000);                       // wait for a second
+			digitalWrite(0, LOW);    // turn the LED off by making the voltage LOW}
+			cli();
+			sleep_enable();
+			set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+			adcsra = ADCSRA;               //save the ADC Control and Status Register A
+			ADCSRA = 0;                    //disable ADC
+			sei();
+			sleep_cpu();                   //go to sleep
+			sleep_disable();               //wake up here
+			ADCSRA = adcsra;               //restore ADCSRA
+		}
+	}
+}
+
+void setVirtualWireForRF433Receiver()
+{
+	vw_set_ptt_pin(999);
+	vw_set_ptt_inverted(false);
+	vw_set_rx_pin(3);
+	vw_setup(100); // Bits per sec
+	vw_rx_start();
+}
---
+++ receiverSample/receiverSample.ino
@@ -0,0 +1,91 @@
+/* 1byte 433Mhz RECEIVER example.
+/* Tutorial link: http://electronoobs.com/eng_arduino_tut99.php
+ * Code: http://electronoobs.com/eng_arduino_tut99_code2.php
+ * Scheamtic: http://electronoobs.com/eng_arduino_tut99_sch1.php
+ * Youtube Channel: http://www.youtube/c/electronoobs
+// Arduino          433Mhz RX
+// GND              GND
+// 5V               VCC
+// D3               Data
+*/
+
+#include <SoftwareSerial.h>
+#include <VirtualWire.h>              //Download it here: http://electronoobs.com/eng_arduino_virtualWire.php
+
+SoftwareSerial softwareSerial(10, 3, false);
+
+//unsigned long _sendMessageTime;
+
+void setup()
+{
+	Serial.begin(9600);
+	softwareSerial.begin(38400);
+	setVirtualWireForRF433Receiver();
+	pinMode(13, OUTPUT);
+	digitalWrite(13, LOW);
+	//_sendMessageTime = millis();
+}
+
+void setVirtualWireForRF433Receiver()
+{
+	vw_set_ptt_pin(999);
+	vw_set_ptt_inverted(false);
+	vw_set_rx_pin(2);
+	vw_setup(100); // Bits per sec
+	vw_rx_start();
+}
+
+
+
+void loop()
+{
+	//_sendMessageTime++;
+	//if (_sendMessageTime > 34000000)
+	//{
+	//	sendMessageToOpenGateIfNoCar();
+	//	_sendMessageTime = 0;
+	//}
+
+
+	////using for uploading the second pcb.
+	//digitalWrite(0, HIGH);
+	//return;
+
+	uint8_t message[1];
+	uint8_t messageLength = 1; // the size of the message
+	vw_wait_rx_max(10000);
+	if (vw_get_message(message, &messageLength)) // non-blocking
+	{
+		for (int i = 0; i < messageLength; i++)
+		{
+			softwareSerial.println((char)message[i]);
+			if ((char)message[i] == 'B')
+			{
+				/*for (int i = 0; i < 2; i++)
+				{*/
+				digitalWrite(13, HIGH);
+				delay(1000);
+				digitalWrite(13, LOW);
+				delay(1000);
+				//}
+				Serial.println("beccato");
+				//_sendMessageTime = 0;
+				//delay(1000);
+			}
+			//received_number = message[i];
+		}
+	}
+	//digitalWrite(13, HIGH);
+	//delay(500);
+	//digitalWrite(13, LOW);
+	//delay(500);
+
+}
+void sendMessageToOpenGateIfNoCar()
+{
+	digitalWrite(0, HIGH);
+	delay(150000);
+	softwareSerial.print("CK1");
+	delay(500000);
+	digitalWrite(0, LOW);
+}
